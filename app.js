a585f5a05ceecd1d355561c30b9e2f89e390768d
spencera0024 <spencera024@gmail.com>
2023-04-10 14:39:11 -0500

    Made a connection to the databse

--- app.js
+++ app.js
@@ -12,6 +12,53 @@ mongoose.connect(connectionString,
 {useNewUrlParser: true,
 useUnifiedTopology: true});
 
+//Get the default connection
+var db = mongoose.connection;
+
+//Bind connection to error event
+db.on('error', console.error.bind(console, 'MongoDB connection error.'));
+db.once("open", function() {
+  console.log("Connection to DB succeeded")
+});
+
+const Gem = require("./models/gems")
+
+// We can seed the collection if needed on server start
+async function recreateDB() {
+  // Delete everything
+  await Gem.deleteMany()
+
+  let instance1 = new Gem({name: "ruby", color: "red", hardness: 9});
+  instance1.save()
+    .then(function (instance1) {
+      console.log(instance1)
+    })
+    .catch(function (err) {
+      console.log(err)
+    }) 
+
+  let instance2 = new Gem({name: "sapphire", color: "blue", hardness: 9});
+  instance2.save()
+    .then(function (instance2) {
+      console.log(instance2)
+    })
+    .catch(function (err) {
+      console.log(err)
+    })
+  
+  let instance3 = new Gem({name: "emerald", color: "green", hardness: 7.5});
+  instance3.save()
+    .then(function (instance3) {
+      console.log(instance3)
+    })
+    .catch(function (err) {
+      console.log(err)
+    })
+
+}
+let reseed = true;
+if (reseed) { recreateDB();}
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var gemsRouter = require('./routes/gems');
