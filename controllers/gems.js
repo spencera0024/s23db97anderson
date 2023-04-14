59022d0966278c861bf741f8e4803fcd3e03c249
spencera0024 <spencera024@gmail.com>
2023-04-14 10:39:00 -0500

    resource for update

--- controllers/gems.js
+++ controllers/gems.js
@@ -48,8 +48,31 @@ exports.gem_delete = function(req, res) {
 };
 
 //Handle Gem update form on PUT
-exports.gem_update_put = function(req, res) {
-    res.send('NOT IMPLEMENTED: Gem update PUT' + req.params.id);
+exports.gem_update_put = async function(req, res) {
+    console.log(`update on id ${req.params.id} with body ${JSON.stringify(req.body)}`);
+    try {
+        let toUpdate = await Gem.findById(req.params.id);
+        //Do updates of properties
+        if(req.body.name) {
+            toUpdate.name = req.body.name;
+        }
+
+        if(req.body.color) {
+            toUpdate.color = req.body.color;
+        }
+
+        if(req.body.hardness) {
+            toUpdate.hardness = req.body.hardness;
+        }
+
+        let result = await toUpdate.save();
+        console.log("Success " + result);
+        res.send(result);
+    } catch (err) {
+        res.status(500);
+        res.send(`{"error": ${err}: Update for id ${req.params.id} failed}`);
+    }
+
 };
 
 //VIEWS 
