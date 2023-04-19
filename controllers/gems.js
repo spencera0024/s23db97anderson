1a2824d0d9030968c2d767e1e9c854dc3fbf9a89
spencera0024 <spencera024@gmail.com>
2023-04-19 14:31:40 -0500

    implemented update page view

--- controllers/gems.js
+++ controllers/gems.js
@@ -118,3 +118,15 @@ exports.gem_create_Page = function(req, res) {
         res.send(`{'error': '${err}'}`);
     }
 }
+
+//Handle building the view for updating a gem
+exports.gem_update_Page = async function(req, res) {
+    console.log("update view for item " + req.query.id);
+    try {
+        let result = await Gem.findById(req.query.id);
+        res.render('gemupdate', {title: 'Gem Update', toShow: result});
+    } catch (err) {
+        res.status(500);
+        res.send(`{'error': '${err}'}`);
+    }
+}
--- routes/gems.js
+++ routes/gems.js
@@ -11,4 +11,7 @@ router.get('/detail', gem_controllers.gem_view_one_Page);
 // GET create gem page 
 router.get('/create', gem_controllers.gem_create_Page);
 
+// GET create update page 
+router.get('/update', gem_controllers.gem_update_Page);
+
 module.exports = router;
